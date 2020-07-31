ee4f5e17397359b7b0019098ca0de4cd96a964df
nooberman <sethu.pradeep96@gmail.com>
2020-07-31 10:43:07 +0530

    Code for downloading files added to the script. (This was done in the command line interface previously)

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,33 @@
+# Downloading and storing data 
+filename <- "Coursera_DS3_Final.zip"
+
+# Checking if archieve already exists.
+if (!file.exists(filename)){
+    fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+    download.file(fileURL, filename, method="curl")
+}  
+
+# Checking if folder exists
+if (!file.exists("UCI HAR Dataset")) { 
+    unzip(filename) 
+}
+
+
+# Assigning Data frames
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+
+
+
+
+
 # Step1: Merging the training and test set to create one data set
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
